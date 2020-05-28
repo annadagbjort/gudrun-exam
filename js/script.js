a4cf61798084ad15791ad05f129c543cbc68d122
karoline rosenfeldt <karosenfeldt@gmail.com>
2020-05-28 11:18:43 +0200

    more fetching

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
 /* FETCHING FOOTER */
 
-function getContact(){
-    fetch("https://designhavn.dk/5Wordpress/wp-json/wp/v2/contact")
-    .then(function (response){
-        return response.json()
-    })
-
-    .then(function (data){
-        const conInfo = data
-        data.forEach(showInfo)
-    })
-}
+fetch("https://designhavn.dk/5Wordpress/wp-json/wp/v2/contact?_embed")
+.then(function (response){
+    reeturn response.json()
+})
+.then(function (data){
+    showInfo(data)
+})
 
 function showInfo(jsonData){
-    console.log(jsonData)
     jsonData.forEach(showInfo)
 }
 
 function showInfo(contact)
-console.log(contact)
 const template = document.querySelector("template").content;
 const clone = template.cloneNode(true);
 
@@ -29,3 +23,5 @@ clone.querySelector(".contact-email").textContent = contact.email_address;
 clone.querySelector(".contact-phone").textContent = contact.phone_number;
 
 clone.querySelector(".contact-location").textContent = contact.location;
+
+document.querySelector(".footer-items-contact").appendChild(clone);
