6f6398b642bd8d2aa62ce19ff69c93db7e21fb35
astridcaecilie <astrid_chr@hotmail.com>
2020-05-28 09:31:43 +0200

    fteching about text and small css changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//window.addEventListener("DOMContentLoaded", init);
 //For quote slideshow
 
 var slideIndex = 1;
@@ -29,3 +30,72 @@ function showSlides(n) {
     slides[slideIndex-1].style.display= "block";
     dots[slideIndex-1].className += " active";
 }
+
+////About page
+//fetch("http://designhavn.dk/5Wordpress/wp-json/wp/v2/posts?_embed&per_page=100")
+//    .then(res => res.json())
+//    .then(handleAboutText)
+
+//function handleAboutText (aboutText) {
+//    aboutText.forEach(showAboutText)
+////    console.log("hej");
+//}
+//
+//function showAboutText (aboutText) {
+////    console.log(aboutText);
+//    const aboutTemplate = document.querySelector("p .center-paragraph").content;
+//
+//    const clone = aboutTemplate.cloneNode(true);
+//    console.log(aboutText.content.rendered)
+//
+//    clone.querySelector(".about-text").innerHtml = aboutText.content.rendered;
+//
+//    document.querySelector(".about-wrapper").appendChild(clone);
+//}
+
+fetch("http://designhavn.dk/5Wordpress/wp-json/wp/v2/posts?_embed&per_page=100")
+    .then(res => res.json())
+    .then(handlePostData)
+
+function handlePostData(frontpagePostHandled) {
+
+    frontpagePostHandled.forEach(item => {
+
+        var theHeader = item.title.rendered;
+        var theContent = item.content.rendered;
+
+
+        item.categories.forEach(categor => {
+
+            //About Gudrun
+            if (categor == 2) {
+                document.querySelector(".about-wrapper .about-header").textContent = theHeader;
+
+                document.querySelector(".about-wrapper .about-text").innerHTML = theContent;
+            }
+
+        });
+    });
+}
+
+
+
+fetch ("http://designhavn.dk/5Wordpress/wp-json/wp/v2/quote?_embed&per_page=100")
+    .then(res => res.json())
+    .then(handleQuotes)
+
+function handleQuotes (quotes) {
+    console.log(quotes);
+ }
+
+
+
+
+
+
+
+
+
+
+
+
