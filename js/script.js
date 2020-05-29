263d76f1d36ac78768e480ba13f62aba559fa167
annadagbjort <annadagbj@gmail.com>
2020-05-29 23:32:16 +0200

    all quotes fetched, many comments

--- js/script.js
+++ js/script.js
@@ -24,29 +24,37 @@ fetch("http://designhavn.dk/5Wordpress/wp-json/wp/v2/quote?_embed&per_page=100")
     .then(res => res.json())
     .then(handleQuotes)
 
+//has comments √
 function handlePostData(frontpagePostHandled) {
 
+    // Now we have an array with all the posts
+    // We need to "look into" the array to see each post "individually"
     frontpagePostHandled.forEach(item => {
 
+        // Find everything I want to fetch from the json and give it a name (put it into a var)
         var theHeader = item.title.rendered;
         var theContent = item.content.rendered;
         var theExcerpt = item.excerpt.rendered;
 
-
+        // Inorder to choose the posts I want to display I look into all the category ids being used
+        // Since I don't aways want to display every post
         item.categories.forEach(categor => {
 
-            // Her Album
+            // Her Album > the category for the text about the album is 6
+            // So now we look through each post and if the category is 6 we add the content we need to html
             if (categor == 6) {
+
+                //We choose where to display the content we need
+                //By selecting elements(by class) in the html document
                 document.querySelector(".debut-album .fp-header").textContent = theHeader;
 
+                //Since in this case wordpress gives us "marked up" text we need to use .innerHTML
                 document.querySelector(".text-about-album").innerHTML = theExcerpt;
-
-
             }
 
-            // Newsletter
+            // Newsletter > the category for the Newsletter is 7
             if (categor == 7) {
-                //                document.querySelector(".newsletter-updates .fp-header").textContent = theHeader;
+                // document.querySelector(".newsletter-updates .fp-header").textContent = theHeader;
 
                 document.querySelector(".text-about-newsletter").innerHTML = theContent;
             }
@@ -55,16 +63,27 @@ function handlePostData(frontpagePostHandled) {
     });
 }
 
+//has comments √
 function handleYoutubeLinkData(youtubeLinks) {
+    //Have an array of all links > need to look into each >>
     youtubeLinks.forEach(TheYoutubeLink => {
 
+        //Have an each link
+
+        //Find everything we need to fetch
         var theHeader = TheYoutubeLink.title.rendered;
         var theVideo = TheYoutubeLink.video_link;
 
+        // Want to filter by category so look into all categories to find what we need
         TheYoutubeLink.categories.forEach(linkCat => {
+
+            // category id of frontpage video is 8
             if (linkCat == 8) {
+
+                // Display the video
                 document.querySelector(".front-page-youtube").src = theVideo;
 
+                //Display the title of the video
                 document.querySelector(".the-video-title-fetched").textContent = theHeader;
             }
         });
@@ -108,6 +127,8 @@ function handleArticleData(ArticleData) {
 
         clonePressTemp.querySelector(".pressImg").src = theArticlePhotoLink;
 
+        clonePressTemp.querySelector(".theArticleLink").href = theArticleLink;
+
 
         document.querySelector(".press").appendChild(clonePressTemp)
 
@@ -116,29 +137,30 @@ function handleArticleData(ArticleData) {
 }
 
 function handleQuotes(quotes) {
-    //    console.log(quotes);
     quotes.forEach(showQuotes)
 }
-
 
 function showQuotes(oneQuote) {
 
     var thePressQuote = oneQuote.quote_content;
     var quotedPerson = oneQuote.quote_author;
 
+
     oneQuote.categories.forEach(quoteCat => {
 
         if (quoteCat == 13) {
+
             const pressQuoteTemp = document.querySelector(".pressQuotesTemp").content;
 
             const clonePressQuoteTemp = pressQuoteTemp.cloneNode(true);
 
-           console.log(oneQuote);
-            pressQuoteTemp.querySelector(".thePressQuote").textContent = thePressQuote;
+            console.log(quotedPerson);
 
-            pressQuoteTemp.querySelector(".quotedPerson").textContent = quotedPerson;
+            clonePressQuoteTemp.querySelector(".thePressQuote").textContent = thePressQuote;
 
-            document.querySelector(".quotes").appendChild(pressQuoteTemp);
+            clonePressQuoteTemp.querySelector(".quotedPerson").textContent = quotedPerson;
+
+            document.querySelector(".quotes").appendChild(clonePressQuoteTemp);
 
         }
     });
