0b8d7de90a00738817616d00dc32b516ccf64354
astridcaecilie <astrid_chr@hotmail.com>
2020-06-01 13:42:07 +0200

    Carousel with new fetching after category and no blank first quote

--- js/script.js
+++ js/script.js
@@ -18,7 +18,11 @@ function handlePostData(frontpagePostHandled) {
 
                 document.querySelector(".about-wrapper .about-text").innerHTML = theContent;
             }
-//            Vocal coaching
+            if (categor == 11) {
+                document.querySelector(".about-wrapper .about-see-more").innerHTML = theContent;
+            }
+
+            //            Vocal coaching
             if (categor == 10) {
                 document.querySelector(".vocal-coaching-wrapper .vocal-coaching-header").textContent = theHeader;
 
@@ -28,27 +32,60 @@ function handlePostData(frontpagePostHandled) {
     });
 }
 
+const seeMoreExpand = document.querySelector(".about-expand");
+const shortAbout = document.querySelector("about-wrapper");
+const longAbout = document.querySelector(".about-see-more");
+longAbout.style.display = "none";
 
-fetch ("http://designhavn.dk/5Wordpress/wp-json/wp/v2/quote?_embed&per_page=100")
+seeMoreExpand.addEventListener("click", showMoreAbout);
+
+function showMoreAbout() {
+    if (longAbout.style.display === "none") {
+        longAbout.style.display = "block";
+        longAbout.classList.add("fade-in");
+        longAbout.classList.remove("fade-out");
+        seeMoreExpand.innerHTML = "Show Less"
+    } else {
+        setTimeout(function () {
+            longAbout.style.display = "none";
+            seeMoreExpand.innerHTML = "Show More";
+        }, 100);
+        longAbout.classList.remove("fade-in");
+        longAbout.classList.add("fade-out");
+    }
+}
+
+//For quote slideshow
+
+
+fetch("http://designhavn.dk/5Wordpress/wp-json/wp/v2/quote?_embed&per_page=100")
     .then(res => res.json())
     .then(handleQuotes)
 
-function handleQuotes (quotes) {
+function handleQuotes(quotes) {
     console.log(quotes);
     quotes.forEach(showQuotes)
- }
-
-function showQuotes (quotes) {
-    console.log(quotes);
-    const quoteTemp = document.querySelector(".quote-template").content;
-    const copy = quoteTemp.cloneNode(true);
-    console.log(quotes.quote_content);
-
-    copy.querySelector(".quote").textContent = quotes.quote_content;
-    copy.querySelector(".author").textContent = quotes.quote_author;
 
 
-    document.querySelector(".slideshow-container").appendChild(copy);
+    function showQuotes(quotesAboutPage) {
+        var aboutQuote = quotesAboutPage.quote_content;
+        var quoteAuthorAbout = quotesAboutPage.quote_author;
+
+        quotesAboutPage.categories.forEach(quoteCategory => {
+            if (quoteCategory == 14) {
+                const aboutQuoteTemp = document.querySelector(".quote-template").content;
+
+                const cloneAboutQuoteTemp = aboutQuoteTemp.cloneNode(true);
+                console.log(quoteAuthorAbout);
+
+                cloneAboutQuoteTemp.querySelector(".quote").textContent = aboutQuote;
+
+                cloneAboutQuoteTemp.querySelector(".author").textContent = quoteAuthorAbout;
+
+                document.querySelector(".slideshow-container").appendChild(cloneAboutQuoteTemp);
+            }
+        });
+    }
 
     const oneSlide = document.querySelector(".mySlides");
     oneSlide.style.display = "block";
@@ -56,11 +93,6 @@ function showQuotes (quotes) {
     const firstDot = document.querySelector(".dot");
     firstDot.classList.add("active");
 }
-
-
-//For quote slideshow
-
-
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -80,30 +112,17 @@ function showSlides(n) {
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
     if (n > slides.length) {
-        slideIndex =1
+        slideIndex = 1
     }
     if (n < 1) {
         slideIndex = slides.length
-    } for (i = 0; i < slides.length; i++) {
+    }
+    for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display= "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
-
-
-
-//
-//To do fredag:
-//
-//    - fetch lesson Text
-//    - ændre citater til rigtige i stedet for fiktiv
-//    - lav responisve design
-//    - Knnapper - hvor skal de føre hen?
-//
-
-
-
